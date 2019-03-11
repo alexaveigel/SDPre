18f51c1ed682cb125d0dcfcf16d2a24771a7d9bb
aveigel <alexaveigel@yahoo.com>
2019-03-11 00:20:35 -0700

    obj2 lab3

---
+++ Labs/Objective2Lab3.java
@@ -0,0 +1,8 @@
+public class Objective2Lab3{
+    public static void main(String[] args){
+      System.out.println("My Favorite Foods Are:");
+      System.out.println("Apple");
+      System.out.println("Banana");
+      System.out.println("Coconut");
+    }
+  }
