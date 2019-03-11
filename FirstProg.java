bc3190e60e331f38f6fd3e665c9cf9c6695f7ff8
aveigel <alexaveigel@yahoo.com>
2019-03-10 23:20:37 -0700

    finish jthw ex 1

---
+++ FirstProg.java
@@ -0,0 +1,6 @@
+public class FirstProg {
+  public static void main( String[] args) {
+    System.out.print.ln( "I am determined to learn how to code.");
+    Sytem.out.print.ln( "Today's date is" );
+  }
+}
