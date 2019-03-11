936e536420dfeb46947c69db1d0bd61f845b5a9b
aveigel <alexaveigel@yahoo.com>
2019-03-11 00:11:37 -0700

    obj1 lab3

---
+++ Labs/Objective1Lab3.java
@@ -0,0 +1,7 @@
+public class Objective1Lab3{
+    public static void main(String[] args){
+      System.out.println("Name: Alexa Veigel");
+      System.out.println("Age: 24");
+      System.out.println("State: Texas");
+    }
+  }
